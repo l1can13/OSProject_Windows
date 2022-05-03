57bb563f2c2e94b6f64e8b5c5ddc9b4458b89aa4
l1can <a.a.soroka05@gmail.com>
2022-05-03 11:42:14 +0400

    trying to do listview

--- OSProject/MainWindow.xaml.cs
+++ OSProject/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,20 @@ namespace OSProject
         public MainWindow()
         {
             InitializeComponent();
+
+            List<string> filenamesList = new List<string>();
+            filenamesList.Add("hello1");
+            filenamesList.Add("hello2");
+            filenamesList.Add("hello3");
+            filenamesList.Add("hello4");
+            filenamesList.Add("hello5");
+            listViewFiles.ItemsSource = filenamesList;
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
         }
     }
 }
